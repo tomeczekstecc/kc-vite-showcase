c46cba55aedd9ec8946b0fa0eaa0897e35ac883f
Tomasz Steć <tomeczekstecc@gmail.com>
2024-06-23 07:38:30 +0200

    dist for coolify

--- src/kc.ts
+++ src/kc.ts
@@ -1,9 +1,10 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'http://localhost:8080/auth', // Keycloak base URL
-    realm: 'your-realm', // Your realm name
-    clientId: 'my-vite-app' // Your client ID
+    url: 'https://my-kc.codderzz.com/auth', // Keycloak base URL
+    realm: 'myrealm', // Your realm name
+    clientId: 'kc-vite' // Your client ID
+
 });
 
 export default keycloak;
